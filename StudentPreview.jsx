fffa2d4c569021a30905953401fef8f48e3c6b3e
ankitkumar04100 <ankitkumarforall@gmail.com>
2025-09-21 16:49:32 +0530

    Create StudentPreview.jsx

---
+++ StudentPreview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StudentPreview = () => {
+  return (
+    <div className="mb-5 p-4 bg-white shadow rounded">
+      <h2 className="text-xl font-semibold mb-2">Student Preview</h2>
+      <p>Teachers can see exercises exactly as students will see them.</p>
+    </div>
+  );
+};
+
+export default StudentPreview;
+
